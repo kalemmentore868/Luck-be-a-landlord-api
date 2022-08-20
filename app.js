0e977f71cf7aa760629767fcbb37c14a3d4405df
kalemmentore868 <kalemmalek123@gmail.com>
2022-08-20 16:19:06 -0400

    fixed port

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.get("*", function (req, res) {
   });
 });
 
-app.listen(3000, () => {
-  console.log("app is listening on Port 3000...");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.warn(`App listening on http://localhost:${PORT}`);
 });
